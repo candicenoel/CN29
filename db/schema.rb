cceaa573278c8ad1f182bcb2611c1afa808405ef
candicenoel <candice29@gmail.com>
2019-08-06 20:20:33 +0000

    Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2019_07_19_024403) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "quotes", force: :cascade do |t|
+  create_table "quotes", id: :serial, force: :cascade do |t|
     t.string "saying"
     t.string "author"
     t.datetime "created_at", null: false
